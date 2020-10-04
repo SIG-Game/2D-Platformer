af468655d01c798c943c92d9690dacfec37d9fc4
adenprince <45126168+adenprince@users.noreply.github.com>
2020-10-04 13:32:30 -0500

    Added test scene and basic player movement
    A test level, horizontal movement, and a jump have been added.
    The player can currently jump in midair because no ground check is implemented.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Set in the Unity Editor
+    public float maxSpeed;
+    public float jumpVelocity;
+
+    // Defined by the script
+    Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get player Rigidbody2D
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get left and right input (-1.0f to 1.0f)
+        float horizontal = Input.GetAxis("Horizontal");
+
+        // Velocities to be applied to the player
+        float horizVel = horizontal * maxSpeed;
+        float vertVel = rb2d.velocity.y;
+
+        // Check if the jump button has just been pressed
+        if (Input.GetButtonDown("Jump"))
+        {
+            // Set new vertical velocity to jump velocity
+            vertVel = jumpVelocity;
+        }
+
+        // Apply velocities to the player
+        rb2d.velocity = new Vector2(horizVel, vertVel);
+    }
+}
