9223f1223739a9a77cd956a7289171914782920c
Dinesh2908 <60485014+Dinesh2908@users.noreply.github.com>
2020-10-09 16:50:13 -0500

    Add files via upload

---
+++ bullet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    public float speed = 20f;
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    void onHit(Collider2D HIT)
+    {
+        Debug.Log(HIT.name);
+        Destroy(gameObject);
+    }
+}
---
+++ weapon.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weapon : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletpre;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            shoot();
+        }
+    }
+
+    void shoot ()
+    {
+        Instantiate(bulletpre, firePoint.position,firePoint.rotation);
+    }
+}
