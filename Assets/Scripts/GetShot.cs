84913f0d3d469f5a6921dc2a93021e620f254d9e
Zlrobbins9 <zlrobbins9@gmail.com>
2020-10-23 21:08:21 -0500

    Added Dummy
    Added a dummy that can be shot and has health

---
+++ Assets/Scripts/GetShot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetShot : MonoBehaviour
+{
+
+    public int health = 3;
+    void Update() {
+        
+    }
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        //Debug.Log("Trigger collision detected. Collision name = " + coll.gameObject.name);
+        Destroy(coll.gameObject);
+        health -= 1;
+        if (health == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
